835e98895250654e2903f117e84db2c6ad5c5dd5
Mixa1997 <43315299+Mixa1997@users.noreply.github.com>
2020-11-25 17:15:50 +0200

    Add files via upload

---
+++ Cikl_c_par_3.1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cikl_c_par_3._1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int x = 0;
+            int m = 56;
+            for (; m != 0; m /= 10)
+            {
+                int r = m % 10;
+                if (r > x) x = r;
+            }
+        }
+    }
+}
